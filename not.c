825ef61021a6d89d0716c6a5a861fd9b66eb7c2a
eray-c <73905858+eray-c@users.noreply.github.com>
2020-12-10 23:30:47 +0300

    Update not.c

--- not.c
+++ not.c
@@ -3,37 +3,17 @@
 #include <string.h>
 #include "not.h"
 
-int sayiOku() {
-    /*Sistemde kaç tane öğrenci kayıtlı olduğunu sayi.txt dosyasından okuyoruz.*/
-    int sayi1;
-    FILE* sayiPtr = NULL;
-    sayiPtr = fopen("sayi.txt","r");
-    if(sayiPtr==NULL) {
-        printf("\nSayi listesine ulaşılamadı!\n");
-        return 0;
-    }
-    fscanf(sayiPtr, "%d", &sayi1);
-    fclose(sayiPtr);
-    
-    return sayi1;
-}
-void ogrenciOku(Ogrenci ogrenci[],int sayi) {
-    /*Sistemde kayıtlı olan öğrencilerin listesini ogrenci.txt dosyasından ogrenci dizisine aktarıyoruz.*/
-    FILE* filePtr = NULL;
-    filePtr = fopen("ogrenci.txt","r");
-    if(filePtr==NULL) {
-        printf("\nÖğrenci listesine ulaşılamadı!\n");
-        return;
-    }
-    int i;
-    for(i=0; i<sayi; i++) {
-        fscanf(filePtr, "%s %s %s %s %s %d %d %d",ogrenci[i].num,ogrenci[i].tc,ogrenci[i].ad,ogrenci[i].soyad,ogrenci[i].sinif,&ogrenci[i].sinav1,
-                &ogrenci[i].sinav2,&ogrenci[i].sinav3);
-    }
-    fclose(filePtr);
-}
-void notEkle(Ogrenci ogrenci[],int sayi) {
+static int sayiOku();
+static void ogrenciOku(Ogrenci[] ,int );
+static void sayiGuncelle(int );
+static void ogrGuncelle(Ogrenci[],int );
+static int sorgu();
+
+void notEkle() {
     printf("\n[NOT EKLEME EKRANI]\n");
+    int sayi = sayiOku();
+    Ogrenci ogrenci[sayi];
+    ogrenciOku(ogrenci,sayi);
     char num[12];
     char secim;
     int ara,vize,finnal;
@@ -65,11 +45,16 @@ void notEkle(Ogrenci ogrenci[],int sayi) {
         default:
             printf("Lütfen geçerli bir işlem seçiniz!\n");
             break;
-    }    
+    }
+    sayiGuncelle(sayi);
+    ogrGuncelle(ogrenci,sayi);    
     } else
         printf("\nÖğrenci bulunamadı.\n");
 }
-void notGoruntule(Ogrenci ogrenci[],int sayi) {
+void notGoruntule() {
+    int sayi = sayiOku();
+    Ogrenci ogrenci[sayi];
+    ogrenciOku(ogrenci,sayi);
     printf("\n[NOT SORGULAMA EKRANI]\n");
     int sira = sorgu(ogrenci,sayi);
     if(sira>-1) {
@@ -80,29 +65,35 @@ void notGoruntule(Ogrenci ogrenci[],int sayi) {
     } else
          printf("Kayit bulunamadi.\n");    
 }
-void ekle(Ogrenci ogrenci[],int sayi) {
+void ekle() {
+    int sayi = sayiOku();
+    Ogrenci ogrenci[sayi];
+    ogrenciOku(ogrenci,sayi);
     printf("\n[ÖĞRENCİ KAYIT EKRANI]\n");
     sayi++;
-    Ogrenci* ptr = ogrenci;
-    ptr = realloc(ptr, sayi * sizeof(Ogrenci));
+    Ogrenci* filePtr = &ogrenci[sayi];
     
     printf("Öğrencinin adını giriniz : ");
-    scanf("%s",ogrenci[sayi-1].ad);
+    scanf("%s",filePtr->ad);
     printf("Öğrencinin soyadını giriniz : ");
-    scanf("%s",ogrenci[sayi-1].soyad);
+    scanf("%s",filePtr->soyad);
     printf("Öğrencinin TC sini giriniz : ");
-    scanf("%s",ogrenci[sayi-1].tc);
+    scanf("%s",filePtr->tc);
     printf("Öğrencinin numarasını giriniz : ");
-    scanf("%s",ogrenci[sayi-1].num);
+    scanf("%s",filePtr->num);
     printf("Öğrencinin sınıfını giriniz : ");
-    scanf("%s",ogrenci[sayi-1].sinif);
-    ogrenci[sayi-1].sinav1,ogrenci[sayi-1].sinav2,ogrenci[sayi-1].sinav3 = 0;
+    scanf("%s",filePtr->sinif);
+    filePtr->sinav1,filePtr->sinav2,filePtr->sinav3 = 0;
     printf("Kayit başarıyla oluşturulmuştur.\n");
-    free(ptr);
+    sayi++;
+    free(filePtr);
     sayiGuncelle(sayi);
     ogrGuncelle(ogrenci,sayi);
 }
-void sil(Ogrenci ogrenci[],int sayi) {
+void sil() {
+    int sayi = sayiOku();
+    Ogrenci ogrenci[sayi];
+    ogrenciOku(ogrenci,sayi);
     printf("\n[KAYIT SİLME EKRANI]\n");
     Ogrenci* filePtr;
     int sira = sorgu(ogrenci,sayi);
@@ -117,7 +108,10 @@ void sil(Ogrenci ogrenci[],int sayi) {
     sayiGuncelle(sayi);
     ogrGuncelle(ogrenci,sayi);
 }
-int sorgu(Ogrenci ogrenci[],int sayi) {
+static int sorgu() {
+    int sayi = sayiOku();
+    Ogrenci ogrenci[sayi];
+    ogrenciOku(ogrenci,sayi);
     /*Kullanıcıdan öğrenci numarasını okuyup ogrenci dizisindeki öğrenci numaralarıyla karşılaştırıyoruz.Girilen numara sistemde kayıtlıysa
      numaranın bulunduğu indisi döndürüyoruz.Kayıtlı değilse -1 değerini döndürüyoruz.*/
     int i;
@@ -132,7 +126,7 @@ int sorgu(Ogrenci ogrenci[],int sayi) {
     }
     return -1;
 }
-void sayiGuncelle(int sayi) {
+static void sayiGuncelle(int sayi) {
     /*Tüm işlemler sonrasında sayi.txt dosyasındaki değeri güncelliyoruz.*/
     FILE* filePtr = NULL;
     filePtr = fopen("sayi.txt","w+");
@@ -143,7 +137,7 @@ void sayiGuncelle(int sayi) {
     fprintf(filePtr,"%d",sayi);
     fclose(filePtr);
 }
-void ogrGuncelle(Ogrenci ogrenci[],int sayi) {
+static void ogrGuncelle(Ogrenci ogrenci[],int sayi) {
     /*Tüm işlemler sonrasında ogrenci.txt dosyasındaki öğrenci listesini güncelliyoruz.*/
     FILE* filePtr = NULL;
     filePtr = fopen("ogrenci.txt","w+");
@@ -155,6 +149,35 @@ void ogrGuncelle(Ogrenci ogrenci[],int sayi) {
     for(i=0; i<sayi; i++) {
         fprintf(filePtr,"%s %s %s %s %s %d %d %d",ogrenci[i].num,ogrenci[i].tc,ogrenci[i].ad,ogrenci[i].soyad,ogrenci[i].sinif,ogrenci[i].sinav1,
                 ogrenci[i].sinav2,ogrenci[i].sinav3);
+    }
+    fclose(filePtr);
+}
+static int sayiOku() {
+    /*Sistemde kaç tane öğrenci kayıtlı olduğunu sayi.txt dosyasından okuyoruz.*/
+    int sayi1;
+    FILE* sayiPtr = NULL;
+    sayiPtr = fopen("sayi.txt","r");
+    if(sayiPtr==NULL) {
+        printf("\nSayi listesine ulaşılamadı!\n");
+        return 0;
+    }
+    fscanf(sayiPtr, "%d", &sayi1);
+    fclose(sayiPtr);
+    
+    return sayi1;
+}
+static void ogrenciOku(Ogrenci ogrenci[],int sayi) {
+    /*Sistemde kayıtlı olan öğrencilerin listesini ogrenci.txt dosyasından ogrenci dizisine aktarıyoruz.*/
+    FILE* filePtr = NULL;
+    filePtr = fopen("ogrenci.txt","r");
+    if(filePtr==NULL) {
+        printf("\nÖğrenci listesine ulaşılamadı!\n");
+        return;
+    }
+    int i;
+    for(i=0; i<sayi; i++) {
+        fscanf(filePtr, "%s %s %s %s %s %d %d %d",ogrenci[i].num,ogrenci[i].tc,ogrenci[i].ad,ogrenci[i].soyad,ogrenci[i].sinif,&ogrenci[i].sinav1,
+                &ogrenci[i].sinav2,&ogrenci[i].sinav3);
     }
     fclose(filePtr);
 }
